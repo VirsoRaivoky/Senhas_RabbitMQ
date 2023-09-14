5aa88b55429d631f9b7aaeea1c312063e7a28b13
VirsoRaivoky <wilson_xavier@hotmail.com.br>
2023-09-14 17:29:48 -0300

    Receiver

---
+++ SenhaR/SenhaR/Form1.Designer.cs
@@ -0,0 +1,100 @@
+﻿namespace SenhaR
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.registro = new System.Windows.Forms.RichTextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // registro
+            // 
+            this.registro.Location = new System.Drawing.Point(180, 69);
+            this.registro.Name = "registro";
+            this.registro.Size = new System.Drawing.Size(414, 282);
+            this.registro.TabIndex = 0;
+            this.registro.Text = "";
+            this.registro.TextChanged += new System.EventHandler(this.registro_TextChanged);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Segoe UI", 20F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.label1.Location = new System.Drawing.Point(228, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(304, 46);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Registro de Senhas";
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Segoe UI", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.button1.Location = new System.Drawing.Point(256, 373);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(276, 52);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "Chamar Próximo";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(54, 101);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(50, 20);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "label2";
+            this.label2.Click += new System.EventHandler(this.label2_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.registro);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private RichTextBox registro;
+        private Label label1;
+        private Button button1;
+        private Label label2;
+    }
+}
---
+++ SenhaR/SenhaR/Form1.cs
@@ -0,0 +1,118 @@
+using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+using System.Text;
+using System.Windows.Forms;
+
+
+namespace SenhaR
+{
+    public partial class Form1 : Form
+    {
+        private IConnection connection;
+        private IModel channel;
+        private IModel respostaChannel;
+
+        private int senhaAtual;
+
+        private List<int> senhaPadrao = new List<int>();
+        private List<int> senhaPreferencial = new List<int>();
+
+
+        public Form1()
+        {
+            InitializeComponent();
+            InitializeRabbitMQ();
+        }
+
+        private void InitializeRabbitMQ()
+        {
+            var factory = new ConnectionFactory { HostName = "localhost" };
+            connection = factory.CreateConnection();
+
+            channel = connection.CreateModel();
+            respostaChannel = connection.CreateModel();
+
+            channel.QueueDeclare(
+                queue: "senhas",
+                durable: false,
+                exclusive: false,
+                autoDelete: false,
+                arguments: null);
+
+            respostaChannel.QueueDeclare(
+                queue: "senha_resposta",
+                durable: false,
+                exclusive: false,
+                autoDelete: false,
+                arguments: null);
+
+            //Receber Senha
+            var consumer = new EventingBasicConsumer(channel);
+            consumer.Received += (model, ea) =>
+            {
+                var body = ea.Body.ToArray();
+                var message = Encoding.UTF8.GetString(body);
+
+                senhaAtual++;
+
+                if(message == "Preferencial")
+                {
+                    senhaPreferencial.Add(senhaAtual);
+                }
+                else
+                {
+                    senhaPadrao.Add(senhaAtual);
+                }
+
+                
+                registro.Invoke((MethodInvoker)(() =>
+                {
+                    registro.AppendText($"{senhaAtual} " + message + Environment.NewLine);
+                }));
+            };
+ 
+            channel.BasicConsume(queue: "senhas", autoAck: true, consumer: consumer);
+        }
+
+        private void Form1_Closed(object sender, System.EventArgs e)
+        {
+            connection.Close();
+        }
+
+
+        private void registro_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            int senha;
+
+            if(senhaPreferencial.Count > 0)
+            {
+                senha = senhaPreferencial[0];
+                senhaPreferencial.RemoveAt(0);
+            }
+            else
+            {
+                senha = senhaPadrao[0];
+                senhaPadrao.RemoveAt(0);
+            }
+
+            var resposta = senha.ToString();
+            var body = Encoding.UTF8.GetBytes(resposta);
+
+            channel.BasicPublish(exchange: string.Empty,
+                routingKey: "senha_resposta",
+                basicProperties: null,
+                body: body);
+
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
